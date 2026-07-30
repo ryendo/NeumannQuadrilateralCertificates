acdc31407cfd661451aff1aa64e43eaee4066039
ryndo <rendo.math@gmail.com>
2026-07-30 22:14:06 +0100

    Fix D4 initial-cover representatives

--- src/qn_d4_is_canonical.m
+++ src/qn_d4_is_canonical.m
@@ -2,8 +2,18 @@ function tf = qn_d4_is_canonical(center)
 % Source-code convention for one representative of each D4 orbit.
 
 c=center(:);
-if ~(c(3)>=-1e-12 && c(4)>=-1e-12), tf=false; return; end
-img=[-c(1);c(3);c(2);c(4)];
-lhs=round([c(1) c(2)]*1e12); rhs=round([img(1) img(2)]*1e12);
-tf=(lhs(1)>rhs(1)) || (lhs(1)==rhs(1) && lhs(2)>=rhs(2));
+a=c(1); b=c(2); cc=c(3); d=c(4);
+images=[a, a,  a,  a, -a, -a, -a, -a; ...
+        b, b, -b, -b, -cc,-cc, cc, cc; ...
+        cc,-cc,cc,-cc, -b,  b, -b,  b; ...
+        d,-d, -d,  d,   d, -d, -d,  d];
+keys=round(images.'*1e12);
+best=keys(1,:);
+for k=2:size(keys,1)
+    first_difference=find(keys(k,:)~=best,1);
+    if ~isempty(first_difference) && keys(k,first_difference)>best(first_difference)
+        best=keys(k,:);
+    end
+end
+tf=isequal(round(c.'*1e12),best);
 end
